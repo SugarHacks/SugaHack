cf32955597a8e7a816fc1ce0c333dd12f228f542
audunthecyborg <cyborg.projects@gmail.com>
2014-07-13 04:39:21 +0200

    script to populate mongo with fake e-mails

---
+++ addEmailsToMongo.js
@@ -0,0 +1,24 @@
+/*jslint node: true */
+/*global */
+
+var MongoClient = require('mongodb').MongoClient,
+    Hashids = require('Hashids'),
+    hashids = new Hashids('iknowthisisntagoodsalt');
+
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+    'use strict';
+    if (err) { return err; }
+    var collection = db.collection('values'),
+        users = collection.findAll(),
+        emails = [
+            'mailinator@ill.be.b.ack',
+            'bruce-mailis@yippe-ki-yeah.com',
+            'sylvester@mail.one'
+        ];
+    users.forEach(function (currentUser) {
+        var randEmail = emails[Math.floor(Math.random() * emails.length)],
+            emailHash = hashids.encrypt(randEmail);
+        currentUser.email = emailHash;
+        console.log(emailHash);
+    });
+});
