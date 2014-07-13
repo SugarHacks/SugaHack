27010b58803157e3ea9f94bab6e079c261617604
audunthecyborg <cyborg.projects@gmail.com>
2014-07-13 04:43:01 +0200

    Another typo...

--- addEmailsToMongo.js
+++ addEmailsToMongo.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
     'use strict';
     if (err) { return err; }
     var collection = db.collection('values'),
-        users = collection.findAll(),
+        users = collection.find({}),
         emails = [
             'mailinator@ill.be.b.ack',
             'bruce-mailis@yippe-ki-yeah.com',
